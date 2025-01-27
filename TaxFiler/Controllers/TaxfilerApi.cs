61b14d73ae6e19173ae0f305f98de760e7f2dce2
Mathias Fritsch <mathias.fritsch.extern@rwe.com>
2025-01-27 07:27:42 +0100

    test context

--- TaxFiler/Controllers/TaxfilerApi.cs
+++ TaxFiler/Controllers/TaxfilerApi.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaxFiler.DB;
 using TaxFiler.Model.Dto;
 using TaxFiler.Service;
 
@@ -8,20 +10,33 @@ namespace TaxFiler.Controllers;
 [Authorize]
 [Route("api/documents")]
 [ApiController]
-public class TaxFilerApi : ControllerBase
+public class TaxFilerApi(TaxFilerContext taxFilerContext,IDocumentService documentService) : ControllerBase
 {
-    private readonly IDocumentService _documentService;
 
-    public TaxFilerApi(IDocumentService documentService)
+    [Route("testcontext")]
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<DocumentDto>>> TestContext(string yearMonth)
     {
-        _documentService = documentService;
-    }
+        string message = "";
+        
+        try
+        {
+            var  documents = await taxFilerContext.Documents.ToListAsync();
+            message = "could read documents";
+        }
+        catch (Exception e)
+        {
+            message = e.ToString();
+        }
 
+        return Ok(message);
+    }
+    
     [HttpGet]
     public async Task<ActionResult<IEnumerable<DocumentDto>>> GetDocuments(string yearMonth)
     {
         var date = Common.GetYearMonth(yearMonth);
-        var documents = await _documentService.GetDocumentsAsync(new DateOnly(date.Year, date.Month, 1));
+        var documents = await documentService.GetDocumentsAsync(new DateOnly(date.Year, date.Month, 1));
         return Ok(documents);
     }
 
